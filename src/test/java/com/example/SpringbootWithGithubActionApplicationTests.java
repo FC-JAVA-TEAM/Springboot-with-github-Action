f0298ce8463f064b6a055b475d42aaf6e08bee5d
Irfan shaikh <techirfan.12@gmail.com>
2024-12-19 17:20:41 +0530

    initial commit

---
+++ src/test/java/com/example/SpringbootWithGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithGithubActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
